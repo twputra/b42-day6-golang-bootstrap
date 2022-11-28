757d01c0bf39a2814875b5071ff5f736b41e64a1
Try Widodo Putra <trywidodoputra@gmail.com>
2022-11-28 09:26:27 +0800

    day6

---
+++ go.mod
@@ -0,0 +1,5 @@
+module 6-bootstrap-golang
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	route := mux.NewRouter()
+
+	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello World"))
+	}).Methods("GET")
+
+	route.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello about"))
+	}).Methods("GET")
+
+	fmt.Println(("Server running on port 5000"))
+	http.ListenAndServe("localhost:5000", route)
+}
